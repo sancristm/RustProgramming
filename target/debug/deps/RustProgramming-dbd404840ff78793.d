6843fc03e32c0e415a04f981969b1f4ccfff2732
sancristm <sancristma@gmail.com>
2024-08-23 11:24:07 +0300

    getting started with rust

---
+++ target/debug/deps/RustProgramming-dbd404840ff78793.d
@@ -0,0 +1,5 @@
+C:\Users\Admin\Desktop\RustProgramming\target\debug\deps\libRustProgramming-dbd404840ff78793.rmeta: src/main.rs
+
+C:\Users\Admin\Desktop\RustProgramming\target\debug\deps\RustProgramming-dbd404840ff78793.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/RustProgramming-ff93e885d2e5d989.d
@@ -0,0 +1,5 @@
+C:\Users\Admin\Desktop\RustProgramming\target\debug\deps\libRustProgramming-ff93e885d2e5d989.rmeta: src/main.rs
+
+C:\Users\Admin\Desktop\RustProgramming\target\debug\deps\RustProgramming-ff93e885d2e5d989.d: src/main.rs
+
+src/main.rs:
